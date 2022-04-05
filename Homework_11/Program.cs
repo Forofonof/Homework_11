f5cc1f60de8e3b006df91a5ecb798268d8594595
Forofonof <DanielResult1@outlook.com>
2022-04-05 22:53:21 +0300

    Fix 1.2

--- Homework_11/Program.cs
+++ Homework_11/Program.cs
@@ -4,9 +4,11 @@
 {
     static void Main(string[] args)
     {
-        int rubToUsd = 85; 
-        int usdToRub = 80; 
-        int rubToEur = 95; 
+        int rubToUsd = 85;
+        double rubToUsdA = 0.0125;
+        int usdToRub = 80;
+        int rubToEur = 95;
+        double rubToEurA = 0.0115;
         int eurToRub = 90;
         double balanceRub = 215000;
         double balanceUsd = 4500;
@@ -36,12 +38,13 @@
                 case "2":
                     Console.WriteLine($"Сколько желаете обменять рублей? Баланс: {balanceRub} рублей.");
                     Console.WriteLine($"Курс обмена: {rubToUsd} рублей за один доллар.");
+
                     currencyCount = Convert.ToInt32(Console.ReadLine());
+
                     if (balanceRub >= currencyCount)
                     {
                         balanceRub -= currencyCount;
                         balanceUsd += currencyCount / rubToUsd;
-
                         Console.WriteLine($"Сделка успешно завершена! Ваш баланс: {balanceRub} рублей. {balanceUsd} долларов.");
                     }
                     else 
@@ -50,11 +53,14 @@
                         Console.WriteLine($"Ваш баланс: {balanceRub} рублей.");
                         Console.WriteLine("Попробуйте еще раз.");
                     }
+
                     break;
                 case "3":
                     Console.WriteLine($"Сколько желаете обменять рублей? Баланс: {balanceRub} рублей.");
                     Console.WriteLine($"Курс обмена: {rubToEur} рублей за один евро.");
+
                     currencyCount = Convert.ToInt32(Console.ReadLine());
+
                     if (balanceRub >= currencyCount)
                     {
                         balanceRub -= currencyCount;
@@ -68,16 +74,18 @@
                         Console.WriteLine($"Ваш баланс: {balanceRub} рублей.");
                         Console.WriteLine("Попробуйте еще раз.");
                     }
+
                     break;
                 case "4":
                     Console.WriteLine($"Сколько желаете обменять долларов? Баланс: {balanceUsd} долларов.");
                     Console.WriteLine($"Курс обмена: {usdToRub} рублей за один доллар.");
+
                     currencyCount = Convert.ToInt32(Console.ReadLine());
+
                     if (balanceUsd >= currencyCount)
                     {
                         balanceUsd -= currencyCount;
-                        balanceRub += currencyCount / usdToRub;
-
+                        balanceRub += currencyCount / rubToUsdA;
                         Console.WriteLine($"Сделка успешно завершена! Ваш баланс: {balanceUsd} Долларов. {balanceRub} рублей.");
                     }
                     else
@@ -86,16 +94,18 @@
                         Console.WriteLine($"Ваш баланс: {balanceUsd} долларов.");
                         Console.WriteLine("Попробуйте еще раз.");
                     }
+
                     break;
                 case "5":
                     Console.WriteLine($"Сколько желаете обменять евро? Баланс: {balanceEur} евро.");
                     Console.WriteLine($"Курс обмена: {eurToRub} рублей за один евро.");
+
                     currencyCount = Convert.ToInt32(Console.ReadLine());
+
                     if (balanceEur >= currencyCount)
                     {
                         balanceEur -= currencyCount;
-                        balanceRub += currencyCount * eurToRub;
-
+                        balanceRub += currencyCount / rubToEurA;
                         Console.WriteLine($"Сделка успешно завершена! Ваш баланс: {balanceEur} евро. {balanceRub} рублей.");
                     }
                     else
@@ -104,6 +114,7 @@
                         Console.WriteLine($"Ваш баланс: {balanceEur} евро.");
                         Console.WriteLine("Попробуйте еще раз.");
                     }
+
                     break;
                 case "6":
                     isUserWantsOut = true;
